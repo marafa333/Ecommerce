bf61fadf008e8fc6c8148db5d5f3edeb1827ba36
marafa333 <medoarafa55@gmail.com>
2025-01-09 19:24:44 +0200

    DB

--- mystore.sql
+++ mystore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2024 at 04:20 PM
+-- Generation Time: Jan 09, 2025 at 06:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -56,15 +56,6 @@ CREATE TABLE `cart_details` (
   `quantity` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `cart_details`
---
-
-INSERT INTO `cart_details` (`product_id`, `ip_address`, `quantity`) VALUES
-(1, '::1', 0),
-(2, '::1', 0),
-(5, '::1', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -87,7 +78,30 @@ INSERT INTO `categories` (`category_id`, `category_title`) VALUES
 (4, 'Books'),
 (5, 'Games'),
 (6, 'phone'),
-(7, 'shoes');
+(7, 'shoes'),
+(8, 'gsjsk');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_pending`
+--
+
+CREATE TABLE `orders_pending` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `invoice_number` int(255) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `order_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders_pending`
+--
+
+INSERT INTO `orders_pending` (`order_id`, `user_id`, `invoice_number`, `product_id`, `quantity`, `order_status`) VALUES
+(1, 1, 2003221827, 1, 1, 'pending');
 
 -- --------------------------------------------------------
 
@@ -117,9 +131,56 @@ CREATE TABLE `products` (
 INSERT INTO `products` (`product_id`, `product_title`, `product_description`, `product_keywords`, `category_id`, `brand_id`, `product_image1`, `product_image2`, `product_image3`, `product_price`, `date`, `status`) VALUES
 (1, 'mango', 'A mango is an edible stone fruit produced by the tropical tree Mangifera indica.', 'mango , A mango , mango fruit', 1, 1, 'mango.jpg', 'imango1.jpg', 'mango2.jpg', '100', '2024-12-03 15:44:05', 'true'),
 (2, 'mango juice', 'A mango is an edible stone fruit produced by the tropical tree Mangifera indica.', 'mango juice , mango , A mango , mango fruit', 3, 1, 'mango_ju1.jpg', 'mango_ju2.jpg', 'mango_ju.jpg', '200', '2024-12-03 15:45:15', 'true'),
-(3, 'iphone', 'The iPhone is a line of smartphones developed and marketed by Apple that run iOS.', 'iphone , The iPhone , smartphones , iOS', 6, 2, 'iphone.jpg', 'iphone1.jpg', 'iphone2.jpg', '33000', '2024-12-03 15:46:06', 'true'),
+(3, 'iphone', 'IPhone is a line of smartphones developed and marketed by Apple .', 'iphone , The iPhone , smartphones , iOS', 6, 2, 'iphone.jpg', 'iphone1.jpg', 'iphone2.jpg', '33000', '2025-01-06 12:42:10', 'true'),
 (4, 'Shoes', 'Shoes adidas is footwear corporation headquartered in Germany.', 'Shoes , Shoes adidas , adidas', 7, 5, 'shoes2.jpg', 'shoes1.jpg', 'shoes.jpg', '500', '2024-12-03 15:46:37', 'true'),
 (5, 'appel', 'An apple is a round, edible fruit produced by an apple tree. An apple is a round.', 'appel , appels', 1, 1, '629544.jpg', '587597.jpg', '596848.jpg', '200', '2024-12-03 15:21:09', 'true');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_orders`
+--
+
+CREATE TABLE `user_orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount_due` int(255) NOT NULL,
+  `invoice_number` int(255) NOT NULL,
+  `total_products` int(255) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `order_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_orders`
+--
+
+INSERT INTO `user_orders` (`order_id`, `user_id`, `amount_due`, `invoice_number`, `total_products`, `order_date`, `order_status`) VALUES
+(1, 1, 100, 2003221827, 1, '2025-01-09 15:29:30', 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_image` varchar(255) NOT NULL,
+  `user_ip` varchar(100) NOT NULL,
+  `user_address` varchar(255) NOT NULL,
+  `user_mobile` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_table`
+--
+
+INSERT INTO `user_table` (`user_id`, `username`, `user_email`, `user_password`, `user_image`, `user_ip`, `user_address`, `user_mobile`) VALUES
+(1, 'mohamed', 'arafa@gmail.com', '$2y$10$SEum3UAgq7Z582UFUzQubeag/xJGCG7inArqxGJ3ohurFieP.dMim', '313199901_3536079799948985_8539534409724520414_n.jpg', '::1', 'Cairo', '01091212943');
 
 --
 -- Indexes for dumped tables
@@ -144,10 +205,28 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`category_id`);
 
 --
+-- Indexes for table `orders_pending`
+--
+ALTER TABLE `orders_pending`
+  ADD PRIMARY KEY (`order_id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `user_orders`
+--
+ALTER TABLE `user_orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -169,13 +248,31 @@ ALTER TABLE `cart_details`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `orders_pending`
+--
+ALTER TABLE `orders_pending`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user_orders`
+--
+ALTER TABLE `user_orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
